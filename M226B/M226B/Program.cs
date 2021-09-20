64d9f24dbd44f2059a9ddc824863d25fed1f0cd1
Wadafacc <70700633+Wadafacc@users.noreply.github.com>
2021-09-20 09:57:51 +0200

    initial commit, innit

---
+++ M226B/M226B/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace M226B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        
+
+
+
+
+
+        public int ReadByte()
+        {
+            return 0;
+        }
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            return 0;
+        }
+        public void WriteByte()
+        {
+
+        }
+        public void Write(byte[] buffer, int offset, int count)
+        {
+
+        }
+        public void Flush()
+        {
+
+        }
+        public void Close()
+        {
+
+        }
+        public long Seek()
+        {
+            return 0;
+        }
+        //public abstract void SetLength(long value);
+    }
+}
