7bcadb7ecba709a37a3bfe2ac439f71560407c9b
Wadafacc <70700633+Wadafacc@users.noreply.github.com>
2021-10-18 16:11:04 +0200

    ertzu

--- M226B/M226B/Program.cs
+++ M226B/M226B/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+using System.IO; 
+
+
 
 namespace M226B
 {
@@ -7,9 +13,33 @@ namespace M226B
     {
         static void Main(string[] args)
         {
+            //define path
+            string filename = @"C:\Data\byteArray.bin";
+
+            //define arrae
+            byte[] byteArrayWrite = { 200, 201, 202, 203, 204, 205, 206, 207 };
+            byte[] byteArrayRead = new byte[byteArrayWrite.Length];
+
+            //create FileStream
+            FileStream fs = new FileStream(filename, FileMode.Create);
+
+            //write to file
+            fs.Write(byteArrayWrite, 0, byteArrayWrite.Length);
+
+            //read from file
+            fs.Position = 0;
+            fs.Read(byteArrayRead, 0, byteArrayRead.Length);
+
+            // output: values of byte array
+            for (int i = 0; i < byteArrayRead.Length; i++)
+            {
+                Console.Write (byteArrayRead[i] + ", ");       
+            }
             
+            //close filestream and delete file
+            fs.Close();
+            File.Delete(filename);
         }
-        
 
 
 
